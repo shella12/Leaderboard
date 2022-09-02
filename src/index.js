bff705d845f454a970cf942cb12eb8c877d1c07e
shella12 <ayeshaarshad4567@gmail.com>
2022-09-02 11:44:09 +0500

    Add game url

--- src/index.js
+++ src/index.js
@@ -5,35 +5,32 @@ import refresh from './refreshScores.js';
 import list from './listDisplay.js';
 
 let urlScore;
+
+const urlScoreFunction = (id) => {
+  urlScore = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ ${
+    id
+  }/scores/`;
+};
+
 idfunc('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/')
-.then((id) => urlScore = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ '+
-id +
-'/scores/')
-.then( () => console.log(urlScore));
+  .then((id) => urlScoreFunction(id))
+  .then(() => console.log(urlScore));
 
-
-const nameInput=document.getElementById('name');
-const scoreInput=document.getElementById('score');
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
 const form = document.getElementById('form');
 
-form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-        const name=nameInput.value;
-        const score=Number(scoreInput.value);
-        nameInput.value='';
-        scoreInput.value='';
-        result(urlScore,name,score);
-    
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const name = nameInput.value;
+  const score = Number(scoreInput.value);
+  nameInput.value = '';
+  scoreInput.value = '';
+  result(urlScore, name, score);
 });
 
-const refreshBtn=document.getElementById('refresh');
+const refreshBtn = document.getElementById('refresh');
 refreshBtn.addEventListener('click', () => {
-    refresh(urlScore)
-    .then( (value) => list(value))
+  refresh(urlScore)
+    .then((value) => list(value));
 });
-
-
-
-
-
-
--- src/postScores.js
+++ src/postScores.js
@@ -1,17 +1,15 @@
+const result = async (urlScore, name, score) => {
+  fetch(urlScore, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name,
+      score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json());
+};
 
-const result = async (urlScore,name,score) =>{
-    const result= await fetch(urlScore, {
-         method: 'POST',
-         body: JSON.stringify({
-             user: name,
-             score: score
-         }),
-         headers: {
-           'Content-type': 'application/json; charset=UTF-8',
-         },
-       })
-         .then((response) => response.json())
-         .then((json) => console.log(json));
- }
-
- module.exports = result;
+module.exports = result;
